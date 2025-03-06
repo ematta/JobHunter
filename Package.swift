08d52500abccef3ce5dca2bdadd5c105128d2c03
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2025-03-06 15:47:11 -0600

    ci: package update

--- Package.swift
+++ Package.swift
@@ -10,9 +10,9 @@ let package = Package(
         .executable(name: "JobHunter", targets: ["JobHunter"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.14.1"),
-        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.4.3"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.3"),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
     ],
     targets: [
         .executableTarget(
